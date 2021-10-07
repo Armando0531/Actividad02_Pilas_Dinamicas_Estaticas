299896631a4b1e4f912ec2375c5e25cffa6aedd4
Armando Enriquez <armandoenriquez0531@gmail.com>
2021-10-07 13:39:44 -0500

    Se agrega una interfaz llamada RentaPeliculas

--- src/PruebaPilas.java
+++ src/PruebaPilas.java
@@ -1,4 +1,41 @@
 
+class Pelicula{
+	private String titulo;
+	private String genero;
+	
+	public Pelicula(String titulo, String genero) {
+		super();
+		this.titulo = titulo;
+		this.genero = genero;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getGenero() {
+		return genero;
+	}
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	
+	@Override
+	public String toString() {
+		return "Pelicula [titulo=" + titulo + ", genero=" + genero + "]";
+	}
+	
+}
+
+interface RentaPeliculas{
+	boolean insetarPeliculaRentada(Pelicula peli);
+	public boolean eliminarPeliculaRectada();
+	public boolean verificarPilaLlena();
+	public boolean verficarPilaVacia();
+	public void mostrarCima();
+}
+
 public class PruebaPilas {
 
 	public static void main(String[] args) {
